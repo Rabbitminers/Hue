fce8c307af0618c5562cd4c6de7f9e430933ee5a
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-16 22:14:24 +0000

    Begun Icon Component View

---
+++ src/components/IconComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="icon">
+        <!-- TODO -->
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'IconComponent',
+    props: {
+        src: {
+            type: String,
+            default: null,
+        },
+        alt: {
+            type: String,
+            default: '',
+        },
+        size: {
+            type: String,
+            default: 'sm',
+            validator(value: string) {
+                return ['xs', 'sm', 'md', 'lg'].includes(value)
+            },
+        },
+        type: {
+            type: String,
+            default: 'bulb'
+        }
+    }
+}
+</script>
+
+<style>
+    #icon {
+        background-color: aliceblue;
+        border-radius: 100%;
+        width: 3em;
+        height: 3em;
+    }
+</style>
