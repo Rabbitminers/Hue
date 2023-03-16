916b3d6b78c19da0c3c6741fc1887a26a54f5dcb
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-16 17:52:20 +0000

    Added New Store Functons

--- src/api/base/store.ts
+++ src/api/base/store.ts
@@ -1,5 +1,44 @@
 import { Preferences } from '@capacitor/preferences';
 import { URL_KEY } from "@/data/constants"
+import { BridgeData } from './types';
+import { BRIDGE_KEY } from '../../data/constants';
+
+export async function storeBridges(bridges: BridgeData[]): Promise<void> {
+	try {
+		if (!bridges || !Array.isArray(bridges)) {
+			throw new Error("Invalid Bridge Data");
+		}
+		const data = bridges.concat(await readBridges());
+		await Preferences.set({
+			key: BRIDGE_KEY,
+			value: JSON.stringify(data)
+		});
+	} catch (error: any) {
+		console.error(`Failed To Store Bridges: ${error.message}`);
+		throw error;
+	}
+}
+
+export async function storeBridge(bridge: BridgeData): Promise<void> {
+	await storeBridges([bridge]);
+}
+
+export async function readBridges(): Promise<BridgeData[]> {
+	try {
+		const sResult = await Preferences.get({
+			 key: BRIDGE_KEY 
+		});
+		if (sResult.value) {
+			const bridges = JSON.parse(sResult.value) as BridgeData[];
+			return bridges;
+		}
+		return [];
+	} catch (error: any) {
+		console.error(`Failed to retrieve bridge addresses: ${error.message}`);
+		throw error;
+	}
+}
+
 
 export async function setBridgeAddresses(urls: string[]) {
 		try {
