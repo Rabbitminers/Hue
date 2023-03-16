4f283ab9e1e459ef533142f41f87da92d9147649
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-16 17:55:01 +0000

    Added Basic Types For Common Functionality

---
+++ src/api/base/types.ts
@@ -0,0 +1,27 @@
+export type LightState = {
+    on: boolean
+    bri: number
+    hue: number
+    sat: number
+    xy: [number, number]
+    ct: number
+    alert: string
+    effect: string
+}
+
+export interface LightData {
+    id: number;
+    state: LightState;
+}
+
+export type DiscoveryBridgeDefinition = {
+    internalipaddress: string,
+    id?: string
+}
+
+export interface BridgeData {
+    id: string;
+    internalipaddress: string,
+    port: number;
+    username: string;
+}
